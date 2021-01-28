a309147d866124cda48da9672246c8abcfcb045f
ITheCon <cksmith@net-ex.no>
2021-01-28 12:25:24 +0100

    Initial commit

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public GameObject player;
+    Vector3 cameraSpacing;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraSpacing = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + cameraSpacing;
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool up = Input.GetKey("w");
+        bool left = Input.GetKey("a");
+        bool right = Input.GetKey("d");
+
+        if (up) 
+        {
+            // Move the object forward along its z axis 1 unit/second.
+            transform.Translate(Vector3.forward * Time.deltaTime);
+        }
+        if (right)
+        {
+            // Move the object to the right along its x axis 2 unit/second.
+            transform.Translate(Vector3.right * Time.deltaTime * 2);
+        }
+        if (left)
+        {
+            // Move the object to the left along its x axis 2 unit/second.
+            transform.Translate(Vector3.left * Time.deltaTime * 2);
+        }
+
+        Debug.Log(transform.position);
+    }
+}
